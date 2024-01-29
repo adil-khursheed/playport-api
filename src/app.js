8de908f60e318be3887f987ba6ea4992287f3550
adil-khursheed <adilkhursheed60@gmail.com>
2024-01-29 15:38:08 +0530

    Add like controller functions

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import videoRoute from "./routes/video.routes.js";
 import tweetRoute from "./routes/tweet.routes.js";
 import subscriptionRoute from "./routes/subscription.routes.js";
 import playlistRoute from "./routes/playlist.routes.js";
+import likeRoute from "./routes/like.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRoute);
@@ -29,5 +30,6 @@ app.use("/api/v1/videos", videoRoute);
 app.use("/api/v1/tweets", tweetRoute);
 app.use("/api/v1/subscriptions", subscriptionRoute);
 app.use("/api/v1/playlist", playlistRoute);
+app.use("/api/v1/likes", likeRoute);
 
 export { app };
